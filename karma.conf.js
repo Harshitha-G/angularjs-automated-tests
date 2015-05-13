9bc8cceb9bc2c0cfe9603c16b183b468acb526ab
Rodolfo Pereira <rodolfo@princiweb.com.br>
2015-05-13 12:44:51 -0300

    progress reporter

--- karma.conf.js
+++ karma.conf.js
@@ -40,13 +40,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['coverage'],
+    reporters: ['progress', 'coverage'],
     
     coverageReporter: {
       type : 'html',
       dir : 'coverage/'
     },
-
 
     // web server port
     port: 9876,
