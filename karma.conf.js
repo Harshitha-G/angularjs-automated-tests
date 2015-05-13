63f1f5e25f47dc4a1d164d73fb350bd4aa01672f
Rodolfo Pereira <rodolfo@princiweb.com.br>
2015-05-13 12:32:58 -0300

    karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -33,13 +33,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/app/**/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['coverage'],
+    
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
 
     // web server port
